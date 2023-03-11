2d2a20676c33286ba4c83926c15b8d82987d2690
ikeryo <sruyrof@icloud.com>
2023-03-11 19:22:53 +0900

    無駄なコード削除

--- src/page-about.php
+++ src/page-about.php
@@ -5,16 +5,8 @@
   <?php get_header(); ?>
 </head>
 
-<body >
+<body>
   <?php get_template_part('includes/header'); ?>
-  <?php get_template_part('includes/lower-mv'); ?>
-  <?php get_template_part('includes/bread'); ?>
-
-  <main class="l-lower-main">
-    <div class="l-inner c-block-editor">
-      <?php the_content() ?>
-    </div>
-  </main>
 
 
   <?php get_template_part('includes/footer'); ?>
